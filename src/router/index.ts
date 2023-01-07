5dd0046d6f7077f6a22234167b1ebd71792abad5
Tom Chauvel <tom.chauvel@hotmail.com>
2023-01-07 21:34:54 +0100

    Beta

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import EditorVue from '@/views/Editor.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Editor',
+      component: EditorVue
+    },
+  ]
+})
+
+export default router
