2b74adaca34ad5f1adbadd3abd69e287df036ea2
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2023-01-21 23:52:24 +0100

    passage à svelte et editorjs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/main.css'
+import "./style.css";
+import App from "./App.svelte";
 
-const app = createApp(App)
+const app = new App({
+  target: document.getElementById("app"),
+});
 
-app.use(router)
-
-app.mount('#app')
+export default app;
