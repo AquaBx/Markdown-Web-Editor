37ef2f5e3aa5622e08dcfd1faa744c4d8f3ce5ce
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2023-02-02 09:55:38 +0100

    update + attente des globals
    attente des paramètres dans globals.ts pour ne pas afficher n'importe quoi

--- src/globals.ts
+++ src/globals.ts
@@ -19,20 +19,20 @@ class datas {
             createDir(datas.contents["directory"])
         }
     } 
-}
-
-(async function() {
-    try{
-        let dir = await appConfigDir()
-        let text = await readTextFile(`${dir}settings.json`)
-        let json = JSON.parse(text)
-        for (let key in json){
-            datas.contents[key] = json[key]
+    public static async load():Promise<boolean>{
+        try{
+            let dir = await appConfigDir()
+            let text = await readTextFile(`${dir}settings.json`)
+            let json:{[key: string]: string} = JSON.parse(text)
+            for (let key in json){
+                if (key in this.contents) this.contents[key] = json[key]
+            }
         }
+        catch(e){
+            datas.save()
+        }
+        return true
     }
-    catch(e){
-        datas.save()
-    }
-})()
+}
 
 export default datas 
