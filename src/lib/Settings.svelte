a926ce3f5e712b70eb2f0cd9e9dac0ec61af7763
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2023-02-04 19:37:00 +0100

    un peu mieux

--- src/lib/Settings.svelte
+++ src/lib/Settings.svelte
@@ -17,5 +17,17 @@
 <style scoped>
     .container > table{
         padding:1cm;
+        width:100%;
+    }
+
+    textarea{
+        background  :unset;
+        font-family: unset;
+        border: 1px solid rgba(255, 255, 255, 0.3);
+        border-radius: 16px;
+        color: var(--text);
+        width: 100%;
+        padding:1em;
+        resize: vertical;
     }
 </style>
